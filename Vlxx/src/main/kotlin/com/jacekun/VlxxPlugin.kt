41df0207a0bea39f6a8d3c44759e568c29035b75
Phatbn11 <100951037+Phatbn11@users.noreply.github.com>
2026-02-01 02:53:09 +0700

    Update VlxxPlugin.kt

--- Vlxx/src/main/kotlin/com/jacekun/VlxxPlugin.kt
+++ Vlxx/src/main/kotlin/com/jacekun/VlxxPlugin.kt
@@ -1,15 +1,11 @@
 package com.phatbn11
 
-import android.content.Context
-import com.byayzen.xVideosExtractor
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class VlxxPlugin: Plugin() {
+class VlxxPlugin : Plugin() {
     override fun load() {
         registerMainAPI(Vlxx())
-        registerExtractorAPI(Vlxx())
     }
-}
 }
